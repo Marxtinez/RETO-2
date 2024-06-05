2677d4a4001d32bb34387d83a2a344772db60c2e
Marcsegsal <marcssalvador@gmail.com>
2024-06-05 20:04:54 +0200

    v 0.1.1

---
+++ src/Model/FCT.java
@@ -0,0 +1,47 @@
+package Model;
+
+public class FCT {
+  private String id_grupo;
+  private String CIF;
+  private String curso;
+  private int num_alumnos;
+
+    public String getId_grupo() {
+        return id_grupo;
+    }
+
+    public void setId_grupo(String id_grupo) {
+        this.id_grupo = id_grupo;
+    }
+
+    public String getCIF() {
+        return CIF;
+    }
+
+    public void setCIF(String CIF) {
+        this.CIF = CIF;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getNum_alumnos() {
+        return num_alumnos;
+    }
+
+    public void setNum_alumnos(int num_alumnos) {
+        this.num_alumnos = num_alumnos;
+    }
+
+    public FCT(String id_grupo, String CIF, String curso, int num_alumnos) {
+        this.id_grupo = id_grupo;
+        this.CIF = CIF;
+        this.curso = curso;
+        this.num_alumnos = num_alumnos;
+    }
+}
---
+++ src/Model/Trabajador.java
@@ -0,0 +1,78 @@
+package Model;
+
+public class Trabajador {
+    private int id_trabajador;
+
+    private String email;
+    private String nombre;
+    private String telefono;
+    private String cargo;
+    private String persona_contacto;
+    private String CIF;
+    public Trabajador(int id_trabajador, String email, String nombre, String telefono, String cargo, String persona_contacto, String CIF){
+        this.id_trabajador=id_trabajador;
+        this.email=email;
+        this.nombre=nombre;
+        this.telefono=telefono;
+        this.cargo=cargo;
+        this.persona_contacto=persona_contacto;
+        this.CIF=CIF;
+
+    }
+
+    public int getId_trabajador() {
+        return id_trabajador;
+    }
+
+    public void setId_trabajador(int id_trabajador) {
+        this.id_trabajador = id_trabajador;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getPersona_contacto() {
+        return persona_contacto;
+    }
+
+    public void setPersona_contacto(String persona_contacto) {
+        this.persona_contacto = persona_contacto;
+    }
+
+    public String getCIF() {
+        return CIF;
+    }
+
+    public void setCIF(String CIF) {
+        this.CIF = CIF;
+    }
+}
