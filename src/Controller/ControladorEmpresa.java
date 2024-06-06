5d27b9fe689eba2196aa9dadc3ffe0b4b06fdf7b
Marxtinez <adri.xico11@gmail.com>
2024-06-06 16:04:29 +0200

    corregido BDConnection y probando conexión con base de datos

--- src/Controller/ControladorEmpresa.java
+++ src/Controller/ControladorEmpresa.java
@@ -1,8 +1,15 @@
 package Controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ControladorEmpresa {
+    private static Connection miConexion;
 
     public static void cargaContenidoEmpresas(){
 
+            miConexion =DBConnection.getConnection("jdbc:postgres://192.168.109.1:6128/DBRETO", "postgres", "Virtual01");
+
     }
+
 }
