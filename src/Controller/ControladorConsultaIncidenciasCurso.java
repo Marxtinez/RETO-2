8c9d219882ffb2e1c5bc58bc9039926093d9c071
Marcsegsal <marcssalvador@gmail.com>
2024-06-11 17:40:26 +0200

    Consulta C8 modificada

--- src/Controller/ControladorConsultaIncidenciasCurso.java
+++ src/Controller/ControladorConsultaIncidenciasCurso.java
@@ -1,29 +1,48 @@
 package Controller;
-import Model.Empresa_Contacta_Tutor;
+
+import Model.Grupo;
 import Model.Incidencia;
+import Model.IncidenciaGrupo;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
+
 public class ControladorConsultaIncidenciasCurso {
-    // Obtener historial de incidencia por curso
-    public static List<Incidencia> obtenerHistorialContactoPorEmpresa(int id_incidencia) throws SQLException {
-        List<Incidencia> historialIncidenciaPorCurso = new ArrayList<>();
-        String scriptObtenerHistorialContacto = "SELECT * FROM incidencia WHERE id_incidencia = ?";
-        try (PreparedStatement ps = ControladorConexion.miConexion.prepareStatement(scriptObtenerHistorialContacto)) {
-            ps.setInt(1, id_incidencia);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Incidencia historial = new Incidencia(
-                        rs.getInt("id_incidencia"),
-                        rs.getString("descripcion"),
-                        rs.getString("fecha"),
-                        rs.getString("CIF")
-                );
-                historialIncidenciaPorCurso.add(historial);
+    public static ArrayList<IncidenciaGrupo>resultados = new ArrayList<>();
+    public static void cargarConsultaIncidenciasPorCurso(String fechaInicio, String fechaFin) {
+        if (resultados != null) {
+            resultados.clear();
+        }
+
+        String scriptObtenerIncidenciasPorCurso = "SELECT i.id_incidencia, i.descripcion, i.fecha, i.CIF, g.id_grupo, g.num_alumnos, g.id_ciclo " +
+                "FROM incidencia i " +
+                "JOIN grupo g ON i.id_grupo = g.id_grupo " +
+                "WHERE i.fecha BETWEEN ? AND ?";
+
+        try {
+            PreparedStatement statement = ControladorConexion.miConexion.prepareStatement(scriptObtenerIncidenciasPorCurso);
+            statement.setString(1, fechaInicio);
+            statement.setString(2, fechaFin);
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    int id_incidencia = rs.getInt("id_incidencia");
+                    String descripcion = rs.getString("descripcion");
+                    String fecha = rs.getString("fecha");
+                    String CIF = rs.getString("CIF");
+
+                    String id_grupo = rs.getString("id_grupo");
+                    int num_alumnos = rs.getInt("num_alumnos");
+                    String id_ciclo = rs.getString("id_ciclo");
+
+                    Grupo grupo = new Grupo(id_grupo, num_alumnos, id_ciclo);
+                    Incidencia incidencia = new Incidencia(id_incidencia, descripcion, fecha, CIF);
+
+                    IncidenciaGrupo incidenciaGrupo = new IncidenciaGrupo(incidencia, grupo);
+                    resultados.add(incidenciaGrupo);
+                }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return historialIncidenciaPorCurso;
     }
 }
---
+++ src/Model/IncidenciaGrupo.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class IncidenciaGrupo {
+    private Incidencia incidencia;
+    private Grupo grupo;
+
+    public IncidenciaGrupo(Incidencia incidencia, Grupo grupo) {
+        this.incidencia = incidencia;
+        this.grupo = grupo;
+    }
+
+    public Incidencia getIncidencia() {
+        return incidencia;
+    }
+
+    public void setIncidencia(Incidencia incidencia) {
+        this.incidencia = incidencia;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+}
+
