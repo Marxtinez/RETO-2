3e1962bdcbba34bb9289a12000a59c430639a83a
JavierNb04 <javinb04@gmail.com>
2024-06-12 11:31:59 +0200

    tablaC1

--- src/View/PanelC1.java
+++ src/View/PanelC1.java
@@ -1,11 +1,15 @@
 package View;
 
-import Model.Idioma;
+import Controller.ControladorConsultaEmpresaPorCIF;
+import Model.*;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.util.Vector;
 
 public class PanelC1 extends JPanel {
+    JButton btnBusca;
+    JTextField txtBusca;
     JTable tabla;
     public PanelC1() {
         Idioma idioma = new Idioma(Idioma.spanish);
@@ -17,10 +21,24 @@ public class PanelC1 extends JPanel {
         DefaultTableModel tableModel = new DefaultTableModel(columns,0);
         tabla = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(tabla);
+        tabla.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        tabla.getColumnModel().getColumn(1).setPreferredWidth(150);
+        txtBusca = new JTextField();
+        btnBusca = new JButton(idioma.getProperty("buscar"));
 
+        btnBusca.addActionListener(e-> {ControladorConsultaEmpresaPorCIF.cargaConsultaEmpresaC1(txtBusca.getText());
+            for (EmpresaTutorTrabajador consulta : ControladorConsultaEmpresaPorCIF.resultados){
+                Empresa empresa = consulta.getEmpresa();
+                Trabajador trabajador = consulta.getTrabajador();
+                TutorFCT tutorFCT = consulta.getTutorFCT();
+                String[] fila = {empresa.getCIF(), empresa.getNombre(), empresa.getDireccion(), empresa.getTecnologias(), empresa.getSector(), empresa.getTelefono(), String.valueOf(empresa.getNum_empleados()), String.valueOf(empresa.getUlt_anio_colab()), String.valueOf(tutorFCT.getId_tutor()),tutorFCT.getNombre(),tutorFCT.getEmail(),tutorFCT.getTelefono(), String.valueOf(trabajador.getId_trabajador()),trabajador.getNombre(),trabajador.getTelefono(),trabajador.getCargo(), String.valueOf(trabajador.getPersona_contacto())};
+                tableModel.addRow(fila);
+                tableModel.fireTableDataChanged();
+            }});
 
+        add(txtBusca);
+        add(btnBusca);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         add(scrollPane);
     }
 }
